3658df869a31547f797c6a6900772a07139bcae0
volpito <mpj.rigot@gmail.com>
2021-06-01 17:39:02 +0200

    API up

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_article, only: [:show, :update, :destroy]
 
   # GET /articles
@@ -17,27 +17,32 @@ class ArticlesController < ApplicationController
   # POST /articles
   def create
     @article = Article.new(article_params)
-    @article.user_id = current_user.id if current_user
-
-    if @article.save
-      render json: @article, status: :created, location: @article
-    else
-      render json: @article.errors, status: :unprocessable_entity
+    if authenticate_user!
+      if @article.save
+        @article.user_id = current_user.id if current_user
+        render json: @article, status: :created, location: @article
+      else
+        render json: @article.errors, status: :unprocessable_entity
+      end
     end
   end
 
   # PATCH/PUT /articles/1
   def update
-    if @article.update(article_params)
-      render json: @article
-    else
-      render json: @article.errors, status: :unprocessable_entity
+    if current_user == @article.user
+      if @article.update(article_params)
+        render json: @article
+      else
+        render json: @article.errors, status: :unprocessable_entity
+      end
     end
   end
 
   # DELETE /articles/1
   def destroy
-    @article.destroy
+    if current_user == @article.user
+      @article.destroy
+    end
   end
 
   private
